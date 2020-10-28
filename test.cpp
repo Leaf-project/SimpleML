b007faf97a10ecaa46093ebdb1f2c8257948d367
Leaf-project <73220746+Leaf-project@users.noreply.github.com>
2020-10-28 14:32:49 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include "Kernel/c++/head/init.h"
+#include "Kernel/c++/head/compute.h"
+
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		std::cout << "No enougth args\n";
+		std::exit(0);
+	}
+	std::cout << "Loading from " << argv[1] << "\n";
+	Model<double> mdl = load_model<double>(argv[1]);
+	ptr_mdl(&mdl);
+	std::cout << "Compute level " << std::stoi(argv[2]) << "\n";
+	compute<double>(&(mdl.levels[std::stoi(argv[2])]), 0, false);
+	ptr_mdl(&mdl);
+	return 0;
+};
